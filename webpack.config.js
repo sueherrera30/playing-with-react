e86600701b30cf73f6c629682fe384ea4dace973
sue herrera <sueherrera30@gmail.com>
2018-08-22 15:04:15 -0500

    webpack setup and configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// configuring webpack
+// getting tools needed:
+var webpack = require("webpack");
+var path = require("path");
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+// configuration
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
+};
+// exporting confingutation's object
+module.exports = config;
