9599789964524cdc51db0a97efb3fadcea420a98
sue herrera <sueherrera30@gmail.com>
2018-08-29 15:33:52 -0500

    testing components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert("testing :)")
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> hello, Im a pretty newcomponent</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
