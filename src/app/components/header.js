110e69f7da32cc231ffa21b21d8f0125a95cf49e
sue herrera <sueherrera30@gmail.com>
2018-09-10 01:19:38 -0500

    stateless

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-export class Header extends React.Component {
-    render() {
-        return(
-            <div>
-                <h5> I should be the header, but i dont have style yet... </h5>
-                
-            </div>    
-        );
-    }
+export const Header = (props) => {
+    return (
+        <div>
+            <p> queee ondiiiii!!!  </p>
+        </div>
+    );
 }
