d300623ee5fd95e293d464d71d1c4fc248d8d1ce
sue herrera <sueherrera30@gmail.com>
2018-09-09 23:17:25 -0500

    events without render

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 export class Home extends React.Component {
+
+    constructor(props) {
+        super();
+        this.age = props.person.age;
+    }
+    subirEdad() {
+        this.age += 10; 
+        console.log(this.age)
+    }
     render() {
         return (
             <div>
                 <h3> thiss is a little  sweet home</h3>
-                <p> testing  baby logic:</p>
-                { 5 + 2 }
-                {(2 * 12) / 2}
+                <p>me llamo { this.props.person.name } y tengo { }</p>  
+                <button onClick = {this.subirEdad.bind(this) }> sube la edad </button>     
             </div>
         );
     }
+}
+
+Home.propTypes = {
+    age: PropTypes.number,
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,12 +4,18 @@ import { Header } from "./components/header"
 import { Home } from "./components/Home"
 
 class App extends React.Component {
+    
     render() {
+        const person = {
+            name: "susu",
+            hobbies: ["cello","crossfit","mahikari","giving classes"],
+            age: 26,
+        }
         return (
             <div>
                 <Header/>
                 <h1> hello, Im a pretty newcomponent</h1>
-                <Home/>
+                <Home person = { person }/>
             </div>
         );
     }
