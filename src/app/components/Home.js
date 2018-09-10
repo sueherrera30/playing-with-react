37deee71e4ba0a5c35e43bc0625d48781254808a
sue herrera <sueherrera30@gmail.com>
2018-09-10 00:38:11 -0500

    setime with state

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 
 
 export class Home extends React.Component {
-
     constructor(props) {
         super();
         this.state = {
             age: props.person.initialAge,
-            status: "worried",
+            status: 0,
         }
+
+        setInterval(() => {
+            this.setState({
+                status: this.state.status  += 5,
+            });
+        },1000);
+
     }
     subirEdad() {
         console.log(this.state.age)
@@ -17,17 +23,22 @@ export class Home extends React.Component {
             age: this.state.age += 10,
         });
     }
+
     render() {
         return (
             <div>
                 <h3> thiss is a little  sweet home</h3>
-                <p>me llamo { this.state.age } y tengo { }</p>  
+                <p>me llamo { this.state.age } </p>  
+                <p>tengo { this.state.status } novios :3 </p>  
                 <button onClick = {this.subirEdad.bind(this) }> sube la edad </button>     
             </div>
         );
     }
+
 }
+    
+    
 Home.propTypes = {
     age: PropTypes.number,
-    status: PropTypes.string,
+    status: PropTypes.number,
 }
